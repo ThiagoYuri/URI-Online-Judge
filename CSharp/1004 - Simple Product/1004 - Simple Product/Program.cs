4c86785b4de401a8898d6c22b442133bb94707e8
ThiagoYuri <thiagoyuri0109@gmail.com>
2021-11-18 12:52:33 -0300

    Update name directory

---
+++ CSharp/1004 - Simple Product/1004 - Simple Product/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1004___Simple_Product
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prod = "+
+                (Convert.ToInt32(Console.ReadLine()) * Convert.ToInt32(Console.ReadLine())
+                ));
+        }
+    }
+}
---
+++ CSharp/1005 - Average 1/1005 - Average 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1005___Average_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float A = float.Parse(Console.ReadLine());
+            float B = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("MEDIA = "+(((A/3.5)+(B/7.5))).ToString("F5"));
+
+        }
+    }
+}
