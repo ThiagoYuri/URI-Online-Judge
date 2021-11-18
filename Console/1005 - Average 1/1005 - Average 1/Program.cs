b63ee8c724fae2329f5cdad746342f4b3faf3eeb
ThiagoYuri <thiagoyuri0109@gmail.com>
2021-11-18 13:51:40 -0300

    1000 - 1004

---
+++ Console/1005 - Average 1/1005 - Average 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1005___Average_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A = double.Parse(Console.ReadLine());
+            double B = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("MEDIA = "+(((3.5/ A)+(7.5/ B)/2)).ToString("F5"));
+            Console.ReadKey();
+        }
+    }
+}
